5783359249436c41e3ce8fa244295a5d97e16053
Kyle Krupp <kyle.krupp@gmail.com>
2019-12-01 10:55:23 -0500

    deploy to netflify

---
+++ build/precache-manifest.478b17b8e32ae5b2dce301684151fb14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb8f6266bb450586c943",
+    "url": "/portfolio/static/css/main.132030f0.chunk.css"
+  },
+  {
+    "revision": "fb8f6266bb450586c943",
+    "url": "/portfolio/static/js/main.fb8f6266.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "f51c0b7de0ff912226381af87d696e82",
+    "url": "/portfolio/index.html"
+  }
+];
