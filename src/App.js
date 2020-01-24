1681db7c10fa4e8deb914f6a00c74e58f1c51797
Kyle Krupp <kyle.krupp@gmail.com>
2020-01-24 17:16:13 -0500

    map over project library to display projects

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import Sidebar from "./components/sidebar";
 import About from "./components/about";
 import Timeline from "./components/timeline";
 import Projects from "./components/projects";
+import projects from "./projectLibrary";
 
 class App extends Component {
+  state = {
+    projects: {}
+  };
+
+  componentDidMount() {
+    this.setState({ projects });
+    console.log("mounted");
+  }
+
   render() {
     return (
       <div id="colorlib-page">
@@ -14,7 +24,45 @@ class App extends Component {
           <div id="colorlib-main">
             <About />
             <Timeline />
-            <Projects />
+
+            <div>
+              <section className="colorlib-work" data-section="projects">
+                <div className="colorlib-narrow-content">
+                  <div className="row">
+                    <div
+                      className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                      data-animate-effect="fadeInLeft"
+                    >
+                      <span className="heading-meta">My Work</span>
+                      <h2 className="colorlib-heading animate-box">
+                        Recent Projects
+                      </h2>
+                    </div>
+                  </div>
+                  <div className="row">
+                    {Object.keys(this.state.projects).map(key => (
+                      <Projects
+                        details={this.state.projects[key]}
+                        key={key}
+                        index={key}
+                      />
+                    ))}
+                  </div>
+                  <div className="row">
+                    <div className="col-md-12 animate-box">
+                      <p>
+                        <a
+                          href="#"
+                          className="btn btn-primary btn-lg btn-load-more"
+                        >
+                          Load more <i className="icon-reload" />
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/projectLibrary.js
@@ -0,0 +1,52 @@
+const projects = {
+  project1: {
+    name: "Sushi Sunday!",
+    image: "/images/sushi.png",
+    desc:
+      "My favorite food is Sushi, why not demonstrate the power of React state management with a quick web app?",
+    demo: "https://sushi-sunday.firebaseapp.com/",
+    repo: "https://github.com/kyle-krupp/sushi-market",
+    stack: "React, Rebase, Firebase Realtime DB"
+  },
+  project2: {
+    name: "Dev Connect",
+    image: "/images/connect.png",
+    desc:
+      "A small social network platform for developer collaboration & networking.",
+    demo: "https://sushi-sunday.firebaseapp.com/",
+    repo: "https://github.com/kyle-krupp/sushi-market",
+    stack: "React + Redux, Express (REST), Node, Bootstrap"
+  },
+  project3: {
+    name: "Calendar App",
+    image: "/images/calendar.gif",
+    desc: "Quick calendar app created while learning the Vue stack ",
+    demo: "https://vue-cal-423e2.firebaseapp.com/",
+    repo: "https://github.com/kyle-krupp/vue-calendar",
+    stack: "Vue, Vuetify, Firebase "
+  },
+  project4: {
+    name: "Dapresy",
+    image: "/images/dapresy-logo.png",
+    desc:
+      "Architected the development of a WordPress site for Dapresy's revamped go to market strategy.",
+    demo: "https://dapresy.com/",
+    stack: "WordPress, Divi Builder, PHP, JS "
+  },
+  project5: {
+    name: "Train Scheduler",
+    image: "/images/train.gif",
+    desc: "Schedule your trip to Hogwarts with this scheduling app!",
+    demo: "https://dapresy.com/",
+    stack: "jQuery, Moment JS, Firebase Realtime DB "
+  },
+  project6: {
+    name: "Pu Revu",
+    image: "/images/logo_blue.png",
+    desc: "Schedule your trip to Hogwarts with this scheduling app!",
+    demo: "https://dapresy.com/",
+    stack: "jQuery, Materialize, Firebase Realtime DB "
+  }
+};
+
+export default projects;
