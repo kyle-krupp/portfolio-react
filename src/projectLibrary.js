b8eebbf8ee0ae739aa87d75f1d84a14bbf83046b
Kyle Krupp <kyle.krupp@gmail.com>
2020-04-10 21:04:37 -0400

    add clubhouse project

--- src/projectLibrary.js
+++ src/projectLibrary.js
@@ -1,11 +1,19 @@
 const projects = {
+  quickhits: {
+    name: "Quickhits",
+    image: "/images/spring-boot-logo.png",
+    desc: "Set your baseball lineups!!",
+    repo: "https://github.com/kyle-krupp/clubhouse",
+    demo: "http://clubhouse-prod.eba-mppt2gfx.us-east-1.elasticbeanstalk.com/",
+    stack: "Kotlin(Spring Boot 2.0), React PostgreSQL, Docker",
+  },
   springbootdemo: {
     name: "Spring Boot Demo",
     image: "/images/spring-boot-logo.png",
     desc:
       "A simple REST API following an MVC paradigm when demoing the Java Spring Boot framework.",
     repo: "https://github.com/kyle-krupp/spring-boot-demo",
-    stack: "Java(Spring Boot 2.0), Postgres, Docker"
+    stack: "Java(Spring Boot 2.0), Postgres, Docker",
   },
   project2: {
     name: "Dev Connect",
@@ -14,14 +22,14 @@ const projects = {
       "A small social network platform for developer collaboration & networking.",
     demo: "http://developerconnectorapp.herokuapp.com/",
     repo: "https://github.com/kyle-krupp/devconnector",
-    stack: "React + Redux, Express (REST), Node, Bootstrap"
+    stack: "React + Redux, Express (REST), Node, Bootstrap",
   },
   liri: {
     name: "Liri",
     image: "/images/siri.png",
     desc: "A Node JS CLI app that replicates features of Siri",
     repo: "https://github.com/kyle-krupp/liri-node-app",
-    stack: "Node JS "
+    stack: "Node JS ",
   },
   sushi: {
     name: "Sushi Sunday!",
@@ -30,7 +38,7 @@ const projects = {
       "My favorite food is Sushi, why not demonstrate the power of React state management with a quick web app?",
     demo: "https://sushi-sunday.firebaseapp.com/",
     repo: "https://github.com/kyle-krupp/sushi-market",
-    stack: "React, Rebase, Firebase Realtime DB"
+    stack: "React, Rebase, Firebase Realtime DB",
   },
 
   project3: {
@@ -39,7 +47,7 @@ const projects = {
     desc:
       "A Node JS CLI app for managing inventory in an Amazon-like storefront",
     repo: "https://github.com/kyle-krupp/bamazon",
-    stack: "Node JS, MySQL"
+    stack: "Node JS, MySQL",
   },
   project4: {
     name: "Dapresy",
@@ -47,15 +55,15 @@ const projects = {
     desc:
       "Architected the development of a WordPress site for Dapresy's revamped go to market strategy.",
     demo: "https://dapresy.com/",
-    stack: "WordPress, Divi Builder, PHP, JS "
+    stack: "WordPress, Divi Builder, PHP, JS ",
   },
   project5: {
     name: "Train Scheduler",
     image: "/images/train.gif",
     desc: "Schedule your trip to Hogwarts with this scheduling app!",
     demo: "https://dapresy.com/",
-    stack: "jQuery, Moment JS, Firebase Realtime DB "
-  }
+    stack: "jQuery, Moment JS, Firebase Realtime DB ",
+  },
 };
 
 export default projects;
